b25752fbc69154111d90b146d4e10219382840e9
Beka <bektemir.abdymomunov@mail.ru>
2021-02-19 13:09:49 -0500

    added CukesRunner.javaRunner

---
+++ src/test/java/com/libraryAutomation/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package com.libraryAutomation.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
+        features = "src/test/resources/features",
+        glue = "com/cybertek/step_definitions",
+        dryRun = false, // if dryRun is true , it will not run your step_definitions
+        tags = ""
+)
+
+
+public class CukesRunner {
+}
