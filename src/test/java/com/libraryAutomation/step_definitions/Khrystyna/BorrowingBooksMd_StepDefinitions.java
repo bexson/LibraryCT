912135cf0264318ccbb5a3866c73dc77d941ccfd
PhKhrystyna <pihurchristina@gmail.com>
2021-02-19 22:01:31 -0500

    created step definition class for user_story_8

---
+++ src/test/java/com/libraryAutomation/step_definitions/Khrystyna/BorrowingBooksMd_StepDefinitions.java
@@ -0,0 +1,24 @@
+package com.libraryAutomation.step_definitions.Khrystyna;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BorrowingBooksMd_StepDefinitions {
+
+    @Given("User is on the homepage")
+    public void user_is_on_the_homepage() {
+
+    }
+
+
+    @When("User click Borrowing Books module")
+    public void user_click_borrowing_books_module() {
+
+    }
+    @Then("User should see the following column names:")
+    public void user_should_see_the_following_column_names(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+
+}
--- src/test/resources/features/BorrowingBooksModule.feature
+++ src/test/resources/features/BorrowingBooksModule.feature
@@ -1,5 +1,6 @@
 Feature: Library search feature
 
+  @user_story_8
   Scenario: borrowing books table columns names
     Given User is on the homepage
     When User click Borrowing Books module
