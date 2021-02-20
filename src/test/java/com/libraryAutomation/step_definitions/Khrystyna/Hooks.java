9782bd90dbab854c7cca9def2aea40c3a0e13795
PhKhrystyna <pihurchristina@gmail.com>
2021-02-19 23:22:16 -0500

    located webElements for LOGIN page

--- src/test/java/com/libraryAutomation/step_definitions/Khrystyna/Hooks.java
+++ src/test/java/com/libraryAutomation/step_definitions/Khrystyna/Hooks.java
@@ -1,4 +1,19 @@
 package com.libraryAutomation.step_definitions.Khrystyna;
 
+import com.libraryAutomation.utilities.ConfigurationReader;
+import com.libraryAutomation.utilities.Driver;
+import org.junit.Before;
+
 public class Hooks {
+
+    //setUp scenario is logIn
+    @Before
+    public void setUpScenario(){
+        String url = ConfigurationReader.getProperty("logIn_Url");
+        Driver.getDriver().get(url);
+
+        //passing password/email in the email/password fields
+
+    }
+
 }
