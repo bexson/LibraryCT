7345955c238155dfafb87ff77ec61c90043c93b8
Fatih <fatihealan@gmail.com>
2021-02-20 13:33:26 -0500

    Fixed the login class, added browser="chrome" to the configuration.properties

--- src/test/java/com/libraryAutomation/runners/CukesRunner.java
+++ src/test/java/com/libraryAutomation/runners/CukesRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "com/libraryAutomation/step_definitions",
-        dryRun = true, // if dryRun is true , it will not run your step_definitions
+        dryRun = false, // if dryRun is true , it will not run your step_definitions
         tags = "@librarianAddUser"
 )
 
