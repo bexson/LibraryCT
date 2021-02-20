4354a4691552a9ce9288591de67d9c44e3ddb027
Fatih <fatihealan@gmail.com>
2021-02-20 13:08:39 -0500

    created my feature file, step definition class

---
+++ src/test/java/com/libraryAutomation/step_definitions/LibrarianAddUser_Step_Definitions.java
@@ -0,0 +1,30 @@
+package com.libraryAutomation.step_definitions.user_story_4;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LibrarianAddUser_Step_Definitions {
+
+
+
+    @Given("Librarian is on user management page")
+    public void librarian_is_on_user_management_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Librarian clicks Add User button")
+    public void librarianClicksAddUserButton() {
+    }
+
+
+    @And("Librarian enters <{string}> in fullname")
+    public void librarianEntersInFullname(String arg0) {
+    }
+
+    @And("Librarian chooses <{string}> for usergroup option")
+    public void librarianChoosesForUsergroupOption(String arg0) {
+
+    }
+}
