d733544d5b9384c9d033ba7c0b19e25945002eee
Beka <bektemir.abdymomunov@mail.ru>
2021-02-21 01:37:42 -0500

    added before method in Hooks.java to login as librarian for us5

--- src/test/java/com/libraryAutomation/step_definitions/Hooks.java
+++ src/test/java/com/libraryAutomation/step_definitions/Hooks.java
@@ -1,18 +1,32 @@
 package com.libraryAutomation.step_definitions;
 
+import com.libraryAutomation.pages.BasePage;
+import com.libraryAutomation.pages.LoginPage;
 import com.libraryAutomation.utilities.BrowserUtils;
+import com.libraryAutomation.utilities.ConfigurationReader;
 import com.libraryAutomation.utilities.Driver;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
+    LoginPage loginPage = new LoginPage();
+    BasePage basePage = new BasePage();
+    String url = ConfigurationReader.getProperty("logIn_Url");
+
+    //THIS BEFORE WITH VALUE @records is for USER STORY 5 ( RECORDS DROPDOWN FEATURE )
+    @Before(value = "@records")
+    public void loginAsLibrarian() {
+        Driver.getDriver().get(url);
+        loginPage.loginAsLibrarian();
+    }
 
     @After(order = 2)
     public void tearDownScenario2(Scenario scenario) {
 
-        if(scenario.isFailed()) {
+        if (scenario.isFailed()) {
             byte[] screenShot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenShot, "image/png", scenario.getName());
         }
