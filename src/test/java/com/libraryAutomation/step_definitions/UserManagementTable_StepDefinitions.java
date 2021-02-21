a7ec6c779dd51d86465d9555fd0d3846cbdaa77d
BayanaSab <bayana90@yahoo.com>
2021-02-20 19:11:47 -0500

    Bayana started the project

---
+++ src/test/java/com/libraryAutomation/step_definitions/UserManagementTable_StepDefinitions.java
@@ -0,0 +1,30 @@
+package com.libraryAutomation.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UserManagementTable_StepDefinitions {
+
+
+    @Given("the user\\(librarian) on the homepage")
+    public void the_user_librarian_on_the_homepage() {
+
+    }
+
+
+
+
+    @When("the user click users module")
+    public void the_user_click_users_module() {
+
+    }
+
+
+
+    @Then("the user should see the following column names:")
+    public void the_user_should_see_the_following_column_names(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+
+}
---
+++ src/test/resources/features/UserManagementTable.feature
@@ -0,0 +1,13 @@
+Feature: User management table
+
+  @userManagementTable
+  Scenario: user management table columns names
+    Given the user(librarian) on the homepage
+    When the user click users module
+    Then the user should see the following column names:
+      | Actions         |
+      | User ID         |
+      | Full Name       |
+      | Email           |
+      | Group           |
+      | Status          |
